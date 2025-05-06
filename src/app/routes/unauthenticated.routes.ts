5ea0ab327959e17ec60f77c99134a1c4507a4677
Leandro Baroni <leandrobaroni2103@hotmail.com>
2025-05-06 01:28:04 -0300

    wip: rota de teste da tela todolist

--- src/app/routes/unauthenticated.routes.ts
+++ src/app/routes/unauthenticated.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { authRoutes } from '../pages/(auth)/auth.routes';
+import { toDoRoutes } from '../pages/to-do-items/to-do.routes';
 
 export const unauthenticatedRoutes: Routes = [
   {
     path: '',
     children: authRoutes
-  }
+  },
+  {
+    path: 'itens',
+    children: toDoRoutes
+  },
 ];
